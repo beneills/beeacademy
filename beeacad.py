58986a34da1b9d2c0caa8ed7da0522c879b34df0
Ben Eills <ben@beneills.com>
2013-02-28 17:12:06 +0000

    Aesthetics: Sorted imports, capitalization, etc.

--- beeacad.py
+++ beeacad.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python
-import urllib
-import urllib2
-import re
 
-import beeminderpy
-import settings
+import datetime
 import httplib
 import json
+import re
 import sys
 import time
-import datetime
+import urllib
+import urllib2
 
 from bs4 import BeautifulSoup
 
-#Opens your CodeAcademy Profile
+import beeminderpy
+import settings
+
+
+
+# Open user's CodeAcademy Profile
 url = settings.CODEACADEMY_PROFILE
 response = urllib2.urlopen(url)
 webContent = response.read()
 
-#Reads the response into BeautifulSoup
+# Read the response into BeautifulSoup
 soup = BeautifulSoup(webContent)
 
-#Finds the User Achievements part of the profile page (there's probably a more elegant way to write this)
+# Finds the User Achievements part of the profile page (there's probably a more elegant way to write this)
 achiev = soup.find("div", {"id": "userAchievements"})
 strong = achiev.em
 
-#Removes the parantheses and gives number of achievements
+# Removes the parantheses and gives number of achievements
 numCode = re.sub(r'[^\w]', '', strong.string)
 numCodeAcademy = int(numCode)
 		
-#Using beeminderpy wrapper, authenticate api
-beeapi=beeminderpy.Beeminder(settings.BEEMINDER_AUTH_TOKEN)
+# Use beeminderpy wrapper to authenticate API
+beeapi = beeminderpy.Beeminder(settings.BEEMINDER_AUTH_TOKEN)
 
-#get timestamp
+# Get timestamp
 timestamp = time.time()
 
-#Create Datapoint
+# Create datapoint
 beeapi.create_datapoint(settings.BEEMINDER_USERNAME,settings.BEEMINDER_GRAPH,timestamp,numCodeAcademy,'CodeAcademy Achievements','true')
 print "Created Datapoint"
